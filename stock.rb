cafcaae5d3065cf23efdd06d49aae1bc3064977a
Ken Lu <ken@seeice.com>
2014-04-21 08:38:35 -0400

    continue with requirement

---
+++ stock.rb
@@ -0,0 +1,43 @@
+module Stock
+  class Stock1
+    attr_accessor :items
+    def import_from_csv file
+      lines = File.readlines(file)
+      @items = lines.split(',')
+    end
+
+    def parse_items
+      @items[1..items.length].each do |item|
+        #TODO need validation
+        rs = Hash.new
+        rs[:id] = item[0]
+        rs[:description] = item[1]
+        rs[:price] = item[2]
+        rs[:cost] = item[3]
+        rs[:price_type] = item[4]
+        rs[:quantity_on_hand] = item[5]
+        rs[:modifiers] = Array.new
+        item[6..item.length].each_with_index do |modifier, index|
+          if index % 2 == 1
+            rs_modifier = Hash.new
+            rs_modifier[:name] = modifier
+          else
+            rs_modifier[:price] = modifier
+            rs[:modifiers] << rs_modifier
+          end
+        end if item.length >= 7
+      end
+
+    end
+
+    def to_json
+
+    end
+
+    def to_xml
+    end
+
+    def to_html
+    end
+  end
+end
